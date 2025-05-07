bf9852d796d60db4b3f8ddf0f6fb29f1ebff7835
NayelyAyol <nayely.ayol@epn.edu.ec>
2025-05-06 22:47:26 -0500

    Ejercicios - Libreria

---
+++ Ejercicio.sql
@@ -0,0 +1,7 @@
+SELECT * FROM libreria.ventas;
+INSERT INTO libreria.ventas (VentaID, LibroID, ClienteID, cantidad, fechaVenta) VALUES
+(201,101, 1, 1, '2023-01-15'),
+(202,102, 2, 2, '2023-02-20'),
+(203,103, 3, 1, '2023-03-10'),
+(204,104, 4, 1, '2023-04-05'),
+(205, 105,5, 3, '2023-05-12');
